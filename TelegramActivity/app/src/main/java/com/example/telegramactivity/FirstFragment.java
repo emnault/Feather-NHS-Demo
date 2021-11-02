3399137e21a70bdaf8d7f56ecf4780629c9152af
Emmy Nault <emmy@Emmys-MacBook-Pro.local>
2021-11-02 14:53:41 +0000

    speech working, code cleaned up - commented code deleted

--- TelegramActivity/app/src/main/java/com/example/telegramactivity/FirstFragment.java
+++ TelegramActivity/app/src/main/java/com/example/telegramactivity/FirstFragment.java
@@ -32,7 +32,6 @@ import com.example.telegramactivity.databinding.FragmentFirstBinding;
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
-    //private Socket socket;
 
     @Override
     public View onCreateView(
@@ -41,56 +40,6 @@ public class FirstFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
-//        SocketConnection sock = new SocketConnection();
-//        sock.connectSocket();
-//        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-//
-//        StrictMode.setThreadPolicy(policy);
-//        try{
-//            Socket s=new Socket("137.195.117.241",65432);
-//            DataOutputStream dout=new DataOutputStream(s.getOutputStream());
-//            dout.writeUTF(" ");
-//            dout.flush();
-//            dout.close();
-//            s.close();
-//        }catch(Exception e){System.out.println(e);}
-
-
-
-//        try {
-//            InetAddress laptopIP = InetAddress.getLocalHost();
-//            Socket clientSocket = new Socket(laptopIP, 65432);
-////            Process proc = new ProcessBuilder("curl", "--data", "' '", "localhost:65432").start();
-//            String command = "curl --data '' localhost:65432";
-//            ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }
-
-//        try {
-//            ServerSocket ss = new ServerSocket(65432);
-//            Socket clientSocket = ss.accept();//establishes connection
-//            Process proc = new ProcessBuilder(args).start();
-//        }catch(Exception e){
-//            System.out.println(e);
-//        }
-
-//        try{
-//            ServerSocket server = new ServerSocket(65432);
-//            Socket client = server.accept();
-//        } catch (IOException e) {
-//            System.out.println("Could not listen on port 65432");
-//            System.exit(-1);
-//        }
-
-//        try{
-//            client = server.accept();
-//        } catch (IOException e) {
-//            System.out.println("Accept failed: 4321");
-//            System.exit(-1);
-//        }
-
-
         return binding.getRoot();
 
     }
@@ -103,22 +52,13 @@ public class FirstFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
-//                try {
-//                    //InetAddress serverAddr = InetAddress.getByName("137.195.117.241");
-//                    Socket socket = new Socket("137.195.117.241", 65432);
-//
-//                } catch (UnknownHostException e1) {
-//                    e1.printStackTrace();
-//                } catch (IOException e1) {
-//                    e1.printStackTrace();
-//                }
-
-                //speak();
-
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        speak();
+                        try{
+                            Socket s=new Socket("192.168.1.243", 65432);
+                            s.close();
+                        }catch(Exception e){System.out.println(e);}
                     }
 
                 }).start();
@@ -134,57 +74,4 @@ public class FirstFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
-    private void speak(){
-        //String command = "curl -w --data '' localhost:65432"; //IP of laptop, port matching port in service.py
-
-//        try {
-//            // Process p = processBuilder.start();
-//            Process process = Runtime.getRuntime().exec("curl -w --data '' 192.168.1.243:65432");
-//            //System.out.println("Curl command: ");
-//            Log.i("Curl", "Curl Command Executed");
-//        } catch(Exception e){
-//            System.out.println(e);
-//        }
-        try{
-            //InetAddress serverAddr = InetAddress.getByName("192.168.1.243");
-            //Socket s=new Socket(serverAddr, 65432);
-            Socket s=new Socket("192.168.1.243", 65432);
-            DataOutputStream dout=new DataOutputStream(s.getOutputStream());
-            dout.writeUTF(" ");
-            dout.flush();
-            dout.close();
-            s.close();
-        }catch(Exception e){System.out.println(e);}
-    }
-
-//    public void request() {
-//
-//        // Instantiate the RequestQueue.
-//        RequestQueue queue = Volley.newRequestQueue(this);
-//        String url ="https://www.google.com";
-//
-//// Request a string response from the provided URL.
-//        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-//                new Response.Listener<String>() {
-//                    @Override
-//                    public void onResponse(String response) {
-//                        // Display the first 500 characters of the response string.
-//                        textView.setText("Response is: "+ response.substring(0,500));
-//                    }
-//                }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                textView.setText("That didn't work!");
-//            }
-//        });
-//
-//// Add the request to the RequestQueue.
-//        queue.add(stringRequest);
-//    }
-
-
-
-
-
 }
