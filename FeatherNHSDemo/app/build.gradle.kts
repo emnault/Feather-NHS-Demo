61e5a1c0c6095aeadec531c3891ca8714612a98b
nault22e <en27@hw.ac.uk>
2024-05-06 10:37:13 +0100

    Initial commit - copy of nhs demo project

---
+++ FeatherNHSDemo/app/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+// Define the Libs object directly in build.gradle.kts
+object Libs {
+    const val robotemiSdk = "com.robotemi:sdk:0.10.81"
+    // Add other dependencies as needed
+}
+
+android {
+    namespace = "com.example.feathernhsdemo"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.feathernhsdemo"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation (Libs.robotemiSdk)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+}
